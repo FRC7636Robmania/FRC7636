2c20cfcf87d09bff6011f4d4c6169be9948da2b2
YuYue <9987jdvrb@gmail.com>
2022-06-11 16:09:35 +0800

    migration: commit before add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,43 @@
-const publicPath = process.env.NODE_ENV === 'production' ? '/FRC7636/' : '/'
+// const publicPath = process.env.NODE_ENV === 'production' ? '/FRC7636/' : '/'
 
 
 module.exports = {
-  publicPath,
+  // publicPath,
+
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+  },
 
   "transpileDependencies": [
     "vuetify"
   ],
 
+  // pluginOptions: {
+  //   prerenderSpa: {
 
-
-  pluginOptions: {
-    prerenderSpa: {
-
-      registry: undefined,
-      renderRoutes: [
-        '/', '/home', '/FIRST', '/Team 7636', '/Contact', '/Outreach',
-        '/Leader', '/Mentor', '/news', '/past-seasons', '/2019DeepSpace',
-        '/2020InfiniteRecharge', '/2021InfiniteRecharge', '/News', '/resource', '/sponsor'],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true
-    }
-  }
+  //     registry: undefined,
+  //     renderRoutes: [
+  //       '/', '/home', '/FIRST', '/Team 7636', '/Contact', '/Outreach',
+  //       '/Leader', '/Mentor', '/news', '/past-seasons', '/2019DeepSpace',
+  //       '/2020InfiniteRecharge', '/2021InfiniteRecharge', '/News', '/resource', '/sponsor'],
+  //     useRenderEvent: true,
+  //     headless: true,
+  //     onlyProduction: true
+  //   }
+  // }
 
 }
